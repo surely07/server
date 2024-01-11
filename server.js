7abe41d1c43326b5fde63b8bf83ebf027140ac11
Kwon Bora <sweetpt88@gmail.com>
2024-01-11 17:17:31 +0900

    데이터 업데이트

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-const express = require("express");
-const axios = require("axios");
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const app = express();
-const port = 8000;
-
-app.get("/", async (req, res) => {
-  try {
-    const { data } = await axios.get(
-      "https://api.steampowered.com/ISteamApps/GetAppList/v2"
-    );
-    res.json(data);
-    // res.send("Hello World!");
-  } catch (error) {
-    console.error("api 호출 중 오류: ", error);
-    res.status(500).send("서버 에러");
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
 });
